d1622104cbd27de0aa2f88b0f5c2c072d5ddd914
Jack Grebenc <grebencjack@gmail.com>
2024-06-04 15:08:12 -0400

    Initial Commit

---
+++ src/components/Timer.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { formatSecondsToJSX } from "../utils/timeFormat";
+
+interface TimerProps {
+  initialSeconds: number;
+  children?: React.ReactNode;
+  onTimerDone: () => void;
+}
+
+const Timer: React.FC<TimerProps> = ({
+  initialSeconds,
+  children,
+  onTimerDone,
+}) => {
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [finished, setFinished] = useState(false);
+
+  useEffect(() => {
+    let interval: number = 0;
+    if (seconds > 0) {
+      interval = window.setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    } else {
+      window.clearInterval(interval);
+      setFinished(true);
+      onTimerDone();
+    }
+    return () => window.clearInterval(interval);
+  }, [seconds]);
+
+  return (
+    <>
+      {formatSecondsToJSX(seconds)}
+      {finished && children}
+    </>
+  );
+};
+
+export default Timer;
---
+++ src/components/WordBox.tsx
@@ -0,0 +1,30 @@
+import "../index.css";
+interface Props {
+  items: string[];
+  highlightIndex: number;
+}
+
+function WordBox({ items, highlightIndex }: Props) {
+  return (
+    <>
+      <div className="box">
+        <p>
+          {items.map((item, index) => (
+            <span
+              className={
+                highlightIndex > index
+                  ? "text-success"
+                  : highlightIndex === index
+                  ? "text-secondary"
+                  : ""
+              }
+            >
+              {item + " "}
+            </span>
+          ))}
+        </p>
+      </div>
+    </>
+  );
+}
+export default WordBox;
